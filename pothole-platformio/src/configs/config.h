f2e7aec7d6b21ea04c7d3541fb9b721b2b90ee19
Abishek Sitaula <abisheksitaula07@gmail.com>
2026-08-25 12:12:14 +0545

    fixed everything

--- pothole-platformio/src/configs/config.h
+++ pothole-platformio/src/configs/config.h
@@ -35,8 +35,11 @@ constexpr int NUM_CLASSES = 4;
 #define SEND_HTTP_ALERT false
 
 // Wi-Fi Credentials
-#define WIFI_SSID     "ALHN-34BA"
-#define WIFI_PASSWORD "CKt4ffm2H9"
+// #define WIFI_SSID     "ALHN-34BA"
+// #define WIFI_PASSWORD "CKt4ffm2H9"
+
+#define WIFI_SSID     "BNBBOYS567_2.4"
+#define WIFI_PASSWORD "BNBBOYS@123"
 
 // Database / API Endpoint
 #define API_SERVER_URL "https://pothole-backend-87ew.onrender.com/api/v1/detections"
